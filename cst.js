6ff8622b849805d8e90d999e25fcc35f0b2e9ad6
meharasbeham <128901764+meharasbeham@users.noreply.github.com>
2023-10-06 22:17:12 +0530

    Add files via upload

---
+++ cst.js
@@ -0,0 +1,48 @@
+var button=document.getElementById("span");
+button.innerHTML=10
+var count=10
+setTimeout(()=>{
+    count=count-1
+    button.innerHTML=count
+    setTimeout(()=>{
+        count=count-1
+        button.innerHTML=count
+        setTimeout(()=>{
+            count=count-1
+            button.innerHTML=count
+            setTimeout(()=>{
+                count=count-1
+                button.innerHTML=count
+                setTimeout(()=>{
+                    count=count-1
+                    button.innerHTML=count
+            
+                setTimeout(()=>{
+                    count=count-1
+                    button.innerHTML=count
+            
+                setTimeout(()=>{
+                    count=count-1
+                    button.innerHTML=count
+                    setTimeout(()=>{
+                        count=count-1
+                        button.innerHTML=count
+                    setTimeout(()=>{
+                        count=count-1
+                        button.innerHTML=count
+            
+                setTimeout(()=>{
+                    count=count-1
+                    button.innerHTML="**!!!Happy Independence Day!!!**"
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+},1000)
+
+},1000)
+},1000)
+},1000)
+},1000)
+
